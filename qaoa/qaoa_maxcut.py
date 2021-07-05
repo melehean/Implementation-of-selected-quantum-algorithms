d58395120e9e87780e09ad58908acfd30be6a790
melehean <michal.siecz04@gmail.com>
2021-07-05 20:00:51 +0200

    Add first QAOA implementation for MaxCut problem

---
+++ qaoa/qaoa_maxcut.py
@@ -0,0 +1,84 @@
+import networkx as nx
+import pennylane as qml
+from pennylane import numpy as np
+import collections
+
+def getGraphFromFile():
+    graph = nx.read_edgelist('qaoa/graph.txt', nodetype=int,
+    data=(('weight',int),), create_using=nx.Graph())
+    return graph
+
+graph = getGraphFromFile()
+qubitNumber = graph.number_of_edges()
+layerNumber = 2
+shotsNumber = 100
+device = qml.device("default.qubit", wires=qubitNumber, shots=shotsNumber)
+
+
+def U_b(beta):
+    for qubit in range(qubitNumber):
+        qml.RX(2*beta, wires=qubit)
+
+def U_c(gamma):
+    for i,j in graph.edges():
+        weight = graph.get_edge_data(i,j)['weight']
+        qml.CNOT(wires=[i,j])
+        qml.RZ(weight*gamma, wires = j)
+        qml.CNOT(wires=[i,j])
+
+def compBasisMeasurement(wires):
+    n_wires = len(wires)
+    return qml.Hermitian(np.diag(range(2 ** n_wires)), wires=wires)
+
+pauli_z = [[1, 0], [0, -1]]
+pauli_z_2 = np.kron(pauli_z, pauli_z, requires_grad=False)
+
+@qml.qnode(device)
+def circuit(gammas, betas, edge=None):
+
+    for i in range(0,qubitNumber):
+        qml.Hadamard(wires=i)
+
+    for i in range(0,layerNumber):
+        U_c(gammas[i])
+        U_b(betas[i])
+
+    if edge is None:
+        return qml.sample(compBasisMeasurement(range(qubitNumber)))
+
+    return qml.expval(qml.Hermitian(pauli_z_2, wires=edge))
+
+def costFunction(params):
+    gammas = params[0]
+    betas = params[1]
+    cost = 0
+    for i,j in graph.edges():
+        weight = graph.get_edge_data(i,j)['weight']
+        cost -= 0.5*weight*(1-circuit(gammas, betas, edge=[i,j]))
+    return cost
+
+def getMaxCutSolution(params):
+    gammas = params[0]
+    betas = params[1]
+    outputDecimal = circuit(gammas, betas)
+    occurrences = collections.Counter(outputDecimal)
+    return occurrences
+
+def getOptimizer(optimizerName="GradientDescent", stepSize=0.01):
+    if optimizerName == "GradientDescent":
+        return qml.GradientDescentOptimizer(stepsize=stepSize)
+    if optimizerName == "Adam":
+        return qml.AdamOptimizer(stepsize=stepSize)
+    if optimizerName == "NesterovMomentum":
+        return qml.NesterovMomentumOptimizer(stepsize=stepSize)
+    if optimizerName == "Adagrad":
+        return qml.AdagradOptimizer(stepsize=stepSize)
+
+if __name__ == '__main__':
+    params = 0.01*np.random.rand(2, layerNumber)
+    optimizer = getOptimizer(optimizerName="Adam", stepSize=0.4)
+    stepNumber = 100
+    for i in range(stepNumber):
+        params = optimizer.step(costFunction, params)
+        print("Results in " + str(i) + " step: " + str(getMaxCutSolution(params)))
+    print("Final results: " + str(getMaxCutSolution(params)))
