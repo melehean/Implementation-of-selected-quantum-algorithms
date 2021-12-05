2b8906c7e3b8f9fb53ecc0b75ae1b3b046d23ed4
melehean <michal.siecz04@gmail.com>
2021-12-05 18:35:58 +0100

    Add non-binary classifier and do some minor fixes

--- qnn/qnn_notbinary.py
+++ qnn/qnn_notbinary.py
@@ -5,10 +5,9 @@ from sklearn import datasets
 from sklearn.utils import shuffle
 from sklearn.preprocessing import minmax_scale
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import roc_curve, auc
-from sklearn.metrics import roc_auc_score
 
-data = datasets.load_iris()
+#data = datasets.load_iris()
+data = datasets.load_wine()
 X = data.data
 Y = data.target
 qubits_number = len(X[0])
@@ -23,9 +22,9 @@ def prepare_layer(weights, r):
         qml.CNOT(wires=[i, (i+r)%qubits_number])
 
 
-def prepare_angles(batch):
+def prepare_angles(_input):
     qml.BasisState(np.zeros(qubits_number), wires=range(qubits_number))
-    for index, value in enumerate(batch):
+    for index, value in enumerate(_input):
         qml.RX(value, wires=index)
 
 
@@ -37,7 +36,7 @@ def circuit(weights, shift_array, batch):
     for index, layer in enumerate(weights):
         prepare_layer(layer, shift_array[index])
 
-    return qml.expval(qml.PauliZ(0))
+    return [qml.expval(qml.PauliZ(0)), qml.expval(qml.PauliZ(1)), qml.expval(qml.PauliZ(2))]
 
 
 def qnn(variables, shift_array, batch):
@@ -49,7 +48,11 @@ def qnn(variables, shift_array, batch):
 def count_loss(predictions, expected_results):
     loss_sum = 0
     for prediction, expected_result in zip(predictions, expected_results):
-        single_loss = (prediction - expected_result) ** 2
+        single_loss = 0
+        prediction = (prediction + 1.0)/2.0
+        single_loss += (prediction[expected_result] - 1) ** 2
+        single_loss += (prediction[(expected_result+1)%3]) ** 2
+        single_loss += (prediction[(expected_result-1)%3]) ** 2
         loss_sum += single_loss
     return loss_sum/len(predictions)
 
@@ -75,16 +78,16 @@ def devide_into_batches(array, batch_size):
     return batch_array
 
 def change_predictions_to_classes(predictions, threshold):
-    for i in range(len(predictions)):
-        if predictions[i] >= threshold:
-            predictions[i] = 1
-        else:
-            predictions[i] = 0
-    return predictions
+    classes = []
+    for predictions_vector in predictions:
+        max_prediction_index = np.argmax(predictions_vector)
+        classes.append(max_prediction_index)
+    return classes
 
-def accuracy(variables, shift_array, input, expected_results, threshold):
-    predictions = get_scores(variables, shift_array, input)
+def accuracy(variables, shift_array, _input, expected_results, threshold):
+    predictions = np.array([qnn(variables, shift_array, x) for x in _input])
 
+    predictions = (predictions + 1.0)/2.0
     predictions = change_predictions_to_classes(predictions, threshold)
 
     accuracy_count = 0
@@ -94,16 +97,11 @@ def accuracy(variables, shift_array, input, expected_results, threshold):
     
     return accuracy_count/len(expected_results)
 
-def get_scores(variables, shift_array, input):
-    predictions = np.array([qnn(variables, shift_array, x) for x in input])
-    predictions = (predictions + 1.0)/2.0
-    return predictions
-
 def main():
     global X, Y, qubits_number
     # get only first two classes
-    X = X[Y<=1]
-    Y = Y[Y<=1]
+    #X = X[Y<=1]
+    #Y = Y[Y<=1]
 
     seed = 0
     np.random.seed(0)
@@ -111,47 +109,39 @@ def main():
     X = minmax_scale(X, feature_range=(0, np.pi))
 
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=seed, shuffle=True, stratify=Y)
-    
     Y_train_scaled = np.empty_like(Y_train)
-    Y_train_scaled[Y_train == 0] = -1
-    Y_train_scaled[Y_train == 1] = +1
-
-    layers_number = 3
-    shift_array = [1,2,3]
-    batch_size = 5
+    layers_number = 4
+    shift_array = [1,2,3,1]
+    batch_size = 140
     batches_number = len(X_train)//batch_size
-    epochs_number = 6
-    
+    epochs_number = 10
     weights = 0.01 * np.random.randn(layers_number, qubits_number, 3)
-    bias = 0.0
-    variables = [weights, bias]
+    bias = np.zeros(3)
+    variables = [weights,bias]
 
     optimizer = qml.AdamOptimizer()
 
-    X_batches = devide_into_batches(X_train, batch_size)
-    Y_batches = devide_into_batches(Y_train_scaled, batch_size)
+    
 
     for epoch in range(epochs_number):
         print("Epoch:", epoch)
             
+        X_shuffled, Y_shuffled = shuffle(X_train, Y_train, random_state=seed)
+        X_batches = devide_into_batches(X_shuffled, batch_size)
+        Y_batches = devide_into_batches(Y_shuffled, batch_size)
+        
         for iteration in range(batches_number):
             batch_cost = lambda help_variables: loss(help_variables, shift_array, X_batches[iteration], Y_batches[iteration])
             variables = optimizer.step(batch_cost, variables)
-            
-            print("Iteration", str(iteration))
+        
+        print("Test cost", loss(variables, shift_array, X_test, Y_test))
+        print("Train cost", loss(variables, shift_array, X_train, Y_train))
 
-    accuracy_test_value = accuracy(variables, shift_array, X_test, Y_test, threshold=0.5)
-    accuracy_train_value = accuracy(variables, shift_array, X_train, Y_train, threshold=0.5)
+        accuracy_test_value = accuracy(variables, shift_array, X_test, Y_test, threshold=0.5)
+        accuracy_train_value = accuracy(variables, shift_array, X_train, Y_train, threshold=0.5)
 
-    print("Accuracy in train set:", accuracy_train_value)
-    print("Accuracy in test set:", accuracy_test_value)
-
-    Y_scores = get_scores(variables, shift_array, X_test)
-    
-    fpr, tpr, thresholds = roc_curve(Y_test, Y_scores)
-    print(fpr)
-    print(tpr)
-    print(thresholds)
+        print("Accuracy in train set:", accuracy_train_value)
+        print("Accuracy in test set:", accuracy_test_value)
 
 if __name__ == '__main__':
     main()
